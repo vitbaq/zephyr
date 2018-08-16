2f095949f9dc3168c81b7d65d9df25d12a806b3e
Vitor Barros <vba@cesar.org.br>
2018-08-16 13:35:39 -0300

    knot: Create data items struct
    Signed-off-by: Vitor Barros <vba@cesar.org.br>

--- subsys/knot/data.h
+++ subsys/knot/data.h
@@ -1,3 +1,31 @@
 /**
  * Put your licence here
  */
+
+#include <zephyr/types.h>
+#include <knot_types.h>
+#include <knot_protocol.h>
+
+struct _data_items {
+	/* KNoT Identifier */
+	u8_t			id;
+
+	/* Schema Values */
+	u8_t			value_type;	// KNOT_VALUE_TYPE_* (int, float, bool, raw)
+	u8_t			unit;		// KNOT_UNIT_*
+	u16_t			type_id;	// KNOT_TYPE_ID_*
+	const char		*name;		// App defined data item name
+
+	/* Control the upper lower message flow */
+	u8_t lower_flag;
+	u8_t upper_flag;
+
+	/* Data Values */
+	knot_value_type		last_data;
+
+	/* Config Values */
+	knot_config		config;	// Flags indicating when data will be sent
+
+	/* Time Values */
+	s64_t			last_timeout;	// Stores the last time the data was sent
+};
