70695f36fedd38ec19a140550c8728b00f3ce90c
Vitor Barros <vba@cesar.org.br>
2018-09-20 15:55:35 -0300

    knot: Create data items struct
    Signed-off-by: Vitor Barros <vba@cesar.org.br>

--- samples/net/knot/src/data.c
+++ samples/net/knot/src/data.c
@@ -4,4 +4,33 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <kernel.h>
+
+#include "knot_protocol.h"
+
 #include "data.h"
+
+#define KNOT_THING_DATA_MAX	3
+
+static struct _data_items {
+	/* KNoT identifier */
+	u8_t			id;
+
+	/* Schema values */
+	u8_t			value_type;
+	u8_t			unit;
+	uint16_t		type_id;
+	const char		*name;
+
+	/* Data values */
+	bool			new_value;
+	knot_value_type		last_value;
+	u8_t			*last_value_raw;
+	u8_t			raw_length;
+
+	/* Config values */
+	knot_config		config;
+
+	/* Time values */
+	u32_t			last_timeout;
+} data_items[KNOT_THING_DATA_MAX];
